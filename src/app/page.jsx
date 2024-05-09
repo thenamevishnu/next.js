cdfba06921c98e153d65baab6810250489f72665
thenamevishnu <mail.vishnumk@gmail.com>
2024-05-09 10:44:40 +0530

    Initial commit from Create Next App

---
+++ src/app/page.jsx
@@ -0,0 +1,11 @@
+import { Fragment } from "react"
+
+const Home = () => {
+    return (
+        <Fragment>
+            <div className="text-red-700">Hello</div>
+        </Fragment>
+    )
+}
+
+export default Home
